ab15dbedcfe00dfbc52364c10b7c18f1bdd03716
Dawson Zhou <dawson.m.zhou@gmail.com>
2025-05-23 01:20:08 +0200

    Fix some bugs with auto-filled slug component.
    - Kept creating drafts when there weren't any changes since set was always being invoked.
    - Didn't handle the existing slug being empty to start.

--- src/components/autoFilledSlugInput.tsx
+++ src/components/autoFilledSlugInput.tsx
@@ -4,11 +4,19 @@ import { set, unset, useFormValue } from 'sanity';
 export function AutoFilledSlugInput(sourceField: string, slugify: (source: string) => string) {
   return (props: any) => {
     const sourceValue = useFormValue([ sourceField ]) as string;
-    const { onChange } = props;
+    const { onChange, value } = props;
 
     useEffect(
         () => {
-          onChange(sourceValue ? set({ _type: 'slug', current: slugify(sourceValue) }) : unset());
+          if (sourceValue) {
+            const slugified = slugify(sourceValue);
+            const currentSlug = value?.current;
+            if (slugified !== currentSlug) {
+              onChange(set({ _type: 'slug', current: slugify(sourceValue) }));
+            }
+          } else if (value && value.current) {
+            onChange(unset());
+          }
         },
         [sourceValue],
     );
