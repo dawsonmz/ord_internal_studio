ff8d50215c194750ac0b77640022907942e729d8
Dawson Zhou <dawson.m.zhou@gmail.com>
2025-08-31 00:38:06 +0200

    Fix slug uniqueness checker for training plans to be season scoped.

--- src/schema/trainingPlanType.ts
+++ src/schema/trainingPlanType.ts
@@ -1,4 +1,4 @@
-import { defineArrayMember, defineField, defineType } from 'sanity';
+import { defineArrayMember, defineField, defineType, useClient } from 'sanity';
 import { AutoFilledSlugInput } from '../components/autoFilledSlugInput';
 import { GroupedReferenceInput } from '../components/groupedReferenceInput';
 import { TrainingModuleListField } from '../components/trainingModuleListField';
@@ -25,6 +25,34 @@ export const trainingPlanType = defineType({
             'training_label',
             (source: string) => source.toLowerCase().replaceAll(' ', '-')
         ),
+      },
+      options: {
+        isUnique: async (slug, context) => {
+          const { document, getClient } = context;
+          const sanityClient = getClient({ apiVersion: '2025-04-15' }).withConfig({ perspective: 'drafts' });
+
+          var id = document!._id;
+          if (id.startsWith('drafts.')) {
+            id = id.substring(7);
+          }
+          
+          const parent = await sanityClient.fetch(
+              `*[_type == "season" && $currentId in training_plans[]->_id] {
+                _id,
+                _originalId,
+                "slugs": training_plans[@->_id != $currentId]->slug.current,
+              }`,
+              { currentId: id },
+          );
+        
+          if (!parent || !parent.length) {
+            return true;
+          } else if (parent.length > 1) {
+            console.warn(`Multiple season documents reference the same training plan ${id}`);
+            return false;
+          }
+          return parent[0].slugs.filter((s: String) => s === slug).length < 1;
+        },
       },
     }),
     defineField({
