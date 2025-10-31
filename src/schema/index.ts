7f3f3e9bcceba10036b4bc28c14d0b061a8bee85
Dawson Zhou <dawson.m.zhou@gmail.com>
2025-10-31 17:35:09 +0100

    Remove module_category type, migrated to module_tag.

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,6 +1,5 @@
 import { imageResourceType } from './imageResourceType';
 import { footageType } from './footageType';
-import { moduleCategoryType } from './moduleCategoryType';
 import { moduleTagType } from './moduleTagType';
 import { moduleType } from './moduleType';
 import { rosterType } from './rosterType';
@@ -11,7 +10,6 @@ import { trainingPlanType } from './trainingPlanType';
 export const schemaTypes = [
   imageResourceType,
   footageType,
-  moduleCategoryType,
   moduleTagType,
   moduleType,
   rosterType,
